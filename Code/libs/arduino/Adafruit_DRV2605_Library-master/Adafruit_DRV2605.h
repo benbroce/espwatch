80e0db360a3832f427f247fa6de18cf983f49663
benbroce3 <benbroce2@gmail.com>
2017-05-25 15:07:50 -0400

    add buzz lib

--- Code/libs/arduino/Adafruit_DRV2605_Library-master/Adafruit_DRV2605.h
+++ Code/libs/arduino/Adafruit_DRV2605_Library-master/Adafruit_DRV2605.h
@@ -16,9 +16,9 @@
 
 
 #if ARDUINO >= 100
- #include "Arduino.h"
+#include "Arduino.h"
 #else
- #include "WProgram.h"
+#include "WProgram.h"
 #endif
 
 #include <Wire.h>
--- Code/libs/micropython/buzz.py
+++ Code/libs/micropython/buzz.py
@@ -1,2 +1,6 @@
 import machine
-i2c = machine.I2C(machine.Pin(), machine.Pin())
+i2c = machine.I2C(machine.Pin(), machine.Pin()) //scl, sda
+
+#check these on Adafruit, may vary by i2c exposure type
+read = i2c.readfrom_mem(address, register, bytesNum)
+i2c.writeto_mem(address, register, bytesNum)
